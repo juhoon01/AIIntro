4f2e28e3326367609b3cc4c23442bdd5be881b93
juhoon01 <80323503+juhoon01@users.noreply.github.com>
2024-06-18 16:18:19 +0900

    Update simpleconvnet2.py

--- hw2/simpleconvnet2.py
+++ hw2/simpleconvnet2.py
@@ -17,6 +17,142 @@ class SimpleConvNet:
         filter_pad = conv_param['pad']
         filter_stride = conv_param['stride']
         input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2*filter_pad) // filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+                                           conv_param['stride'], conv_param['pad'])
+        self.layers['BatchNorm1'] = BatchNormalization(filter_num)
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['BatchNorm2'] = BatchNormalization(hidden_size)
+        self.layers['Relu2'] = Relu()
+        self.layers['Dropout1'] = Dropout(dropout_ratio=0.5)
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+
+        self.last_layer = SoftmaxWithLoss()
+
+        # Batch Normalization 파라미터 초기화
+        self.gamma = {}
+        self.beta = {}
+        self.gamma['gamma1'] = np.ones(filter_num)
+        self.beta['beta1'] = np.zeros(filter_num)
+        self.gamma['gamma2'] = np.ones(hidden_size)
+        self.beta['beta2'] = np.zeros(hidden_size)
+
+        # 각 Batch Normalization 레이어에 파라미터 설정
+        self.layers['BatchNorm1'].gamma = self.gamma['gamma1']
+        self.layers['BatchNorm1'].beta = self.beta['beta1']
+        self.layers['BatchNorm2'].gamma = self.gamma['gamma2']
+        self.layers['BatchNorm2'].beta = self.beta['beta2']
+
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        
+        acc = 0.0
+        
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]
+            tt = t[i*batch_size:(i+1)*batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt) 
+        
+        return acc / x.shape[0]
+
+    def numerical_gradient(self, x, t):
+        loss_w = lambda w: self.loss(x, t)
+
+        grads = {}
+        for idx in (1, 2, 3):
+            grads['W' + str(idx)] = numerical_gradient(loss_w, self.params['W' + str(idx)])
+            grads['b' + str(idx)] = numerical_gradient(loss_w, self.params['b' + str(idx)])
+
+        return grads
+
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        # Batch Normalization에 대한 gradient 추가
+        grads['gamma1'] = self.layers['BatchNorm1'].dgamma
+        grads['beta1'] = self.layers['BatchNorm1'].dbeta
+        grads['gamma2'] = self.layers['BatchNorm2'].dgamma
+        grads['beta2'] = self.layers['BatchNorm2'].dbeta
+
+        return grads
+        
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i+1)]
+            self.layers[key].b = self.params['b' + str(i+1)]
+
+        # Batch Normalization 파라미터 설정
+        self.layers['BatchNorm1'].gamma = self.gamma['gamma1']
+        self.layers['BatchNorm1'].beta = self.beta['beta1']
+        self.layers['BatchNorm2'].gamma = self.gamma['gamma2']
+        self.layers['BatchNorm2'].beta = self.beta['beta2']
+    def __init__(self, input_dim=(1, 28, 28),
+                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+                 hidden_size=100, output_size=10, weight_init_std=0.01):  # 변수명 수정
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
         conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
 
@@ -40,6 +176,7 @@ class SimpleConvNet:
         self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
         self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
         self.layers['Relu2'] = Relu()
+        self.layers['Dropout1'] = Dropout(dropout_ratio=0.5)
         self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
 
         self.last_layer = SoftmaxWithLoss()
@@ -116,4 +253,3 @@ class SimpleConvNet:
         for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
             self.layers[key].W = self.params['W' + str(i+1)]
             self.layers[key].b = self.params['b' + str(i+1)]
-
